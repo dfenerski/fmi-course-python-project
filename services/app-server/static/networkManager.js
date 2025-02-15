ceb5277cf6c75e58b24cf7ac8bc7c015db1e858f
Dimitar Fenerski <dfenerski@gmail.com>
2025-02-15 21:54:44 +0200

    feat(app-server): add some js utility classes

---
+++ services/app-server/static/networkManager.js
@@ -0,0 +1,38 @@
+class NetworkManager {
+    static #getCookie(name) {
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ').reduce((acc, cookie) => {
+            const [key, value] = cookie.split('=');
+            acc[key] = value;
+            return acc;
+        }, {});
+        return cookies[name] || null;
+    }
+
+    static #getCSRFToken() {
+        return this.#getCookie('csrftoken');
+    }
+
+    static async POST(payload, extraHeaders = {}) {
+        const csrfToken = this.#getCSRFToken();
+
+        if (!csrfToken) {
+            throw new Error('CSRF token not found.');
+        }
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken,
+            ...extraHeaders,
+        };
+
+        const response = await fetch(window.location.href, {
+            method: 'POST',
+            headers,
+            credentials: 'same-origin',
+            body: JSON.stringify(payload),
+        });
+
+        return response;
+    }
+}
